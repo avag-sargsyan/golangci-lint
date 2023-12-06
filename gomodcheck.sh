6d3b16336c661a13c22d2a3ccdb6e9266fc5011b
Avag Sargsyan <avagsargsyan88@gmail.com>
2023-12-06 22:08:52 +0400

    exit with error if not allowed dep is found

--- gomodcheck.sh
+++ gomodcheck.sh
@@ -6,9 +6,16 @@ declare -a whitelist=("4d63.com/gocheckcompilerdirectives" "4d63.com/gochecknogl
 # Extract direct dependencies from go.mod, skipping 'require', '(', 'indirect', and ')'
 dependencies=$(awk '/require[[:space:]]+\(/,/\)/ {if ($1 !~ /^require$/ && $1 !~ /^\($/ && $1 !~ /^\)$/ && $0 !~ /indirect$/) print $1}' go.mod)
 
+found=0
+
 # Check each dependency
 for dep in $dependencies; do
     if [[ ! " ${whitelist[@]} " =~ " ${dep} " ]]; then
         echo "Unapproved dependency: $dep"
+        found=1
     fi
 done
+
+if [ $found -eq 1 ]; then
+    exit 1
+fi
