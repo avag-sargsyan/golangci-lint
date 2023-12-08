8e47dd6ed7cfecf323d58f8abad6e8122d26653c
Avag Sargsyan <avagsargsyan88@gmail.com>
2023-12-08 23:53:04 +0400

    change configs 3

--- restrictimports.sh
+++ restrictimports.sh
@@ -3,9 +3,9 @@
 # TODO: move to config file
 prefix="github.com/golangci/golangci-lint"
 root_dir="internal"
-modules=()
+modules=("cache" "errorutil" "pkgcache" "renameio" "robustio")
 aggregator="errorutil"
-allowed_packages=()
+allowed_packages=("handlers" "usecases")
 
 found=0
 generated_marker="Code generated" # TODO: maybe check for file name instead?
